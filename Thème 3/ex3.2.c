254ad34bdc53a103c0af66fa3fe2749ab4c4ce7f
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-24 21:30:02 +0100

    feat: implement sorting function for number input in ex3.2.c

--- Thème 3/ex3.2.c
+++ Thème 3/ex3.2.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+void sort(int numbers[], int *index)
+{
+    for (unsigned int i = 0; i < *index - 1; i++)
+    {
+        for (unsigned int j = i + 1; j < *index; j++)
+        {
+            if (numbers[i] > numbers[j])
+            {
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
     int numbers[1000];
@@ -11,18 +27,25 @@ int main()
     size_t sizeAllocated = 0;
     printf("> ");
     size_t characters;
-    
-    while ((characters = getline(&buff, &sizeAllocated, stdin)) != EOF){
+
+    while ((characters = getline(&buff, &sizeAllocated, stdin)) != EOF)
+    {
         int number = atoi(buff);
         numbers[index++] = number;
+
+        sort(numbers, &index);
+
         printf("Converted %d\n", number);
     }
     numbers[index] = '\0';
 
     int sizeNumbers = sizeof(numbers) / sizeof(numbers[0]);
-    for (unsigned int i = 0; i < sizeNumbers; i++){
-        if (numbers[i] == '\0') break;
-        printf("%d\n", numbers[i]);
+    for (unsigned int i = 0; i < sizeNumbers; i++)
+    {
+        if (numbers[i] == '\0')
+            break;
+        printf("|");
+        printf("%d", numbers[i]);
     }
 
     free(buff);
