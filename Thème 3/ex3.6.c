df7195a9f2b14ab5f482920a4968dbc572fb3ae6
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-25 15:26:31 +0100

    fix: 3.6 warnings

--- Thème 3/ex3.6.c
+++ Thème 3/ex3.6.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-char *mon_strchr(char *chaine, int car){
+char *mon_strchr(char *chaine, int car)
+{
     unsigned int index = 0;
-    while (chaine[index] != '\0') {
-        if (chaine[index] == car){
-            return index;
+    while (chaine[index] != '\0')
+    {
+        if (chaine[index] == car)
+        {
+            return &chaine[index];
         }
         index++;
     }
     return NULL;
 }
-
 int main()
 {
     char *chaine = "Salut tout le monde";
@@ -18,13 +20,14 @@ int main()
 
     char *result = mon_strchr(chaine, car);
 
-    if (result != NULL) {
-        printf("Character '%c' found at position: %d\n",
-               car, result);
+    if (result != NULL)
+    {
+        printf("Character '%c' found at position: %ld\n",
+               car, result - chaine);
     }
-    else {
+    else
+    {
         printf("Character '%c' not found.\n", car);
     }
     return 0;
-
 }
