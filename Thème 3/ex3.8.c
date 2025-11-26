53cecf082b29b332b990558bdc1805849b83e8b8
ggarrone <gabriel.garrone@al-enterprise.com>
2025-11-26 10:13:43 +0100

    feat: implement reading, sorting, and displaying an array of doubles

--- Thème 3/ex3.8.c
+++ Thème 3/ex3.8.c
@@ -3,30 +3,53 @@
 
 int lire_tableau(double *tableau, int max_elem, int *nb_elem)
 {
-    int numbers[1000];
     char *buff = NULL;
-    int index = 0;
-
+    *nb_elem = 0;
     size_t sizeAllocated = 0;
     printf("> ");
     size_t characters;
     while ((characters = getline(&buff, &sizeAllocated, stdin)) != EOF)
     {
         double number = atof(buff);
-        numbers[index++] = number;
+        tableau[*nb_elem] = number;
+        (*nb_elem)++;
         printf("Converted %f\n", number);
+        if (*nb_elem >= max_elem) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void trier_tableau(double *tableau, int nb_elem) {
+    for (unsigned int i = 0; i < nb_elem - 1; i++)
+    {
+        for (unsigned int j = i + 1; j < nb_elem; j++)
+        {
+            if (tableau[i] > tableau[j])
+            {
+                int temp = tableau[i];
+                tableau[i] = tableau[j];
+                tableau[j] = temp;
+            }
+        }
     }
 }
 
-void trier_tableau(double *tableau, int nb_elem) {}
 
-void afficher_tableau(double *tableau, int nb_elem) {}
+void afficher_tableau(double *tableau, int nb_elem) {
+    for (unsigned int i = 0; i < nb_elem; i++) {
+        printf("%f|", tableau[i]);
+    }
+}
 
 int main()
 {   
     int max_elem = 100;
-    double *tableau[max_elem];
-    int *nb_elem = 0;
-    lire_tableau(*tableau, max_elem, nb_elem);
+    double tableau[max_elem];
+    int nb_elem = 0;
+    lire_tableau(tableau, max_elem, &nb_elem);
+    trier_tableau(tableau, nb_elem);
+    afficher_tableau(tableau, nb_elem);
     return 0;
 }
