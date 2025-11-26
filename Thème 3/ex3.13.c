0685096a416536e08bf2235033af1b20d77dfe6f
ggarrone <gabriel.garrone@al-enterprise.com>
2025-11-26 17:07:41 +0100

    3.13 with getline

--- Thème 3/ex3.13.c
+++ Thème 3/ex3.13.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 enum code
 {
@@ -21,25 +22,42 @@ struct noeud
     } u;
 };
 
-
-struct noeud *lire_prefixe (void) {
+struct noeud *lire_prefixe(void)
+{
 
     struct noeud *n = malloc(sizeof(struct noeud));
 
-    char c = getchar();
-    if (isdigit(c)) {
-        n->code = feuille;
-        n->u.valeur = c - '0';
-    } else if (c == '+' || c == '*') {
-        n->code = noeud;
-        n->u.s.fils_gauche = lire_prefixe();
-        n->u.s.fils_droit = lire_prefixe();
+    char *buff = NULL;
+    size_t sizeAllocated = 0;
+    printf("> ");
+    size_t characters = getline(&buff, &sizeAllocated, stdin);
+    if (buff[characters - 1] == '\n') {
+        buff[characters - 1] = '\0';
     }
-    
+
+    for (unsigned int i = 0; i < strlen(buff); i++)
+
+    {
+        char c = buff[i];
+        if (c == ' ') continue;
+        if (isdigit(c))
+        {
+            n->code = feuille;
+            n->u.valeur = c - '0';
+        }
+        else if (c == '+' || c == '*')
+        {
+            n->code = noeud;
+            n->u.s.fils_gauche = lire_prefixe();
+            n->u.s.fils_droit = lire_prefixe();
+        }
+    }
+
     return n;
 }
 
-int main() {
+int main()
+{
     lire_prefixe();
     return 0;
 }
