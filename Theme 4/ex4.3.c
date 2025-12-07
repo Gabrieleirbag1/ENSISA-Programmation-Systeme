0c490449c9ee0fe551ee07ef926e5dfe3de06e57
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-07 13:22:02 +0100

    feat: enhance word retrieval and update logic in linked list implementation

--- Theme 4/ex4.3.c
+++ Theme 4/ex4.3.c
@@ -52,18 +52,52 @@ void initialiser_mots(void)
 
 struct mot *trouver_mot(char text[])
 {
+    struct mot *before = NULL;
+    struct mot *next = tete;
+    while (next != NULL) {
+        if (strcmp(next->texte, text) == 0) {
+            return next;
+        }
+        before = next;
+        next = next->suivant;
+    }
+    next = malloc(sizeof(struct mot));
+    before->suivant = next;
+    next->texte = text;
+    return next;
 }
+
 
 void mettre_a_jour_mot(struct mot *mot)
 {
+    struct mot *before = NULL;
+    struct mot *next = tete;
+    mot->nb_occurrences = 0;
+    while (next != NULL) {
+        if (strcmp(next->texte, mot->texte) == 0) {
+            mot->nb_occurrences += 1;
+            next->nb_occurrences = mot->nb_occurrences;
+        }
+        before = next;
+        next = next->suivant;
+    }
 }
 
 void afficher_mots(void)
 {
+    struct mot *current = tete;
+    while (current != NULL) {
+        printf("Mot: %s, Occurrences: %d\n", current->texte, current->nb_occurrences);
+        current = current->suivant;
+    }
 }
 
 int main () {
     initialiser_mots();
-    printf("Liste des mots initialisée.\n");
+    struct mot *motBeau = trouver_mot("il");
+    printf("Mot trouvé: %s\n", motBeau->texte);
+    mettre_a_jour_mot(motBeau);
+    printf("Nombre d'occurrences de '%s': %d\n", motBeau->texte, motBeau->nb_occurrences);
+    afficher_mots();
     return 0;
 }
