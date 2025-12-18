1c2792d51c66f79db67dfd510c9f1e022f287ae5
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-18 16:22:02 +0100

    update gitignore

---
+++ Theme 12/ex12.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[]){
+    if (argc < 4){
+        printf("Usage ./ex12.2 n p t");
+    }
+
+
+
+
+
+
+    return 0;
+}
