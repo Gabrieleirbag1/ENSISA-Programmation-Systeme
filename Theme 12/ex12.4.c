d502d51e315ac175c36b5ac1fce51531919210eb
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-18 17:17:10 +0100

    ex1.24 version sync

--- Theme 12/ex12.4.c
+++ Theme 12/ex12.4.c
@@ -4,36 +4,47 @@
 #include <pthread.h>
 
 long int count;
+pthread_mutex_t m;
 
-void *compteur(void *arg) {
-    for (unsigned long int i = 0; i < 1000000; i++) {
+void *compteur(void *arg)
+{
+    for (unsigned long int i = 0; i < 1000000; i++)
+    {
+        pthread_mutex_lock(&m);
         count++;
+        pthread_mutex_unlock(&m);
     }
     return NULL;
 }
 
-int main() {
+int main()
+{
     pthread_t tid[MAX];
     int thread_nums[MAX];
     int i;
-    
-    for (i = 0; i < MAX; i++) {
+
+    pthread_mutex_init(&m, NULL);
+
+    for (i = 0; i < MAX; i++)
+    {
         thread_nums[i] = i + 1;
-        if (pthread_create(&tid[i], NULL, &compteur, &thread_nums[i]) == -1) {
+        if (pthread_create(&tid[i], NULL, &compteur, &thread_nums[i]) == -1)
+        {
             perror("pthread_create");
             return 1;
         }
     }
-    
-    for (i = 0; i < MAX; i++) {
-        if (pthread_join(tid[i], NULL) == -1) {
+
+    for (i = 0; i < MAX; i++)
+    {
+        if (pthread_join(tid[i], NULL) == -1)
+        {
             perror("pthread_join");
             return 1;
         }
     }
-    printf("Compteur final: %ld", count);
+    printf("Compteur : %ld", count);
     return 0;
 }
 
 // problème constaté: la valeur du compteur est au delà de 1M (Compteur final: 1309868)
-// 
